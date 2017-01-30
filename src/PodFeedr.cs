fa23b2daf1a9b3cbe24ff5bd46000584a2533095
Andrew Collins <andrewcoll@outlook.com>
2017-01-30 23:14:50 +0000

    init

--- src/PodFeedr.cs
+++ src/PodFeedr.cs
@@ -1,13 +1,30 @@
 using System;
+using System.IO;
 using System.Xml;
 
 namespace PodFeedr
 {
     public class PodFeedr
     {
-        public PodFeedr()
+        private readonly Podcast podcast;
+
+        public PodFeedr(Podcast podcast)
         {
-            
+            if(podcast == null)
+            {
+                throw new ArgumentNullException(nameof(podcast));
+            }
+
+            this.podcast = podcast;
+        }
+
+        /// <summary>
+        /// Write XML stream
+        /// </summary>
+        /// <param name="stream"></param>
+        public void WriteStream(Stream stream)
+        {
+
         }
     }
 }
